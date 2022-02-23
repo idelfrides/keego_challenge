387e4b542fc1e248326721283c2f74ae1a19aeba
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-23 11:34:40 -0300

    create a lib to help building the game

---
+++ utils/libs.py
@@ -0,0 +1,33 @@
+
+from datetime import datetime
+from random import randint
+
+
+def show_info(**kwarg):
+
+    info = f"""
+    ---------------------------------------------------------
+            GAME ROUND : [ {kwarg['round']} ]
+            PLAYER NUMBER --> {kwarg['player']}
+    ----------------------------------------------------------
+    """
+    print_log(info)
+    return
+
+
+def print_log(content):
+
+    log_time = datetime.now()
+    log_time = f'[ {str(log_time)[:19]} ] '
+    real_content = '| '.join([log_time, content])
+
+    print(f'\n{real_content}')
+
+    return
+
+
+def define_player_order():
+    print_log(f'DEFINING THE PLAYER ORDER ...')
+    position = randint(1, 4)
+    # print_log(str(position))
+    return position
