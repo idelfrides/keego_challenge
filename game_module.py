2ac9a416f424fec5222dff440d42a48d5111ebd5
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-23 11:32:13 -0300

    player order defined

--- game_module.py
+++ game_module.py
@@ -1,10 +1,24 @@
+#!src/bin/python
+# encoding: utf-8
 
-def the_game():
-    print(f'STARTING THE GAME')
+from utils.libs import (
+    print_log, show_info,
+    define_player_order
+)
+
+from game_manager.lib_manager import GameManager
+
+from random import randint
 
 
+def run_game(round, player_order):
+
+    show_info(round=round, player=player_order)
 
 
+    # dado = randint(1, 6)
+
+    return
 
 
 
@@ -13,4 +27,20 @@ def the_game():
 
 
 if __name__ == '__main__':
-    the_game()
+
+    ROUNDS = 5
+
+    print_log(f'STARTING THE GAME ...\n')
+    player_order = define_player_order()
+
+    for round in range(1, ROUNDS):
+
+        if player_order > 4:
+            player_order = 1
+
+        run_game(round, player_order)
+
+        player_order += 1  # next player
+
+
+    print_log(f'GOME OVER')
