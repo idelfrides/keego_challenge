f3c350d61ef82ee0690d0a7f699cfef0f5da1b37
idelfrides <idelfridesjorgepapai@gmail.com>
2022-02-24 19:49:27 -0300

    clean methods

--- game_manager/lib_manager.py
+++ game_manager/lib_manager.py
@@ -19,13 +19,12 @@ class PalyerManager(object):
 
 
     def get_land_property(self):
-        manx_rent = 200
-
+        manx_rent = 150
         land_property = {}
         land_property['name'] = ''
         land_property['owner'] = self.player
-        land_property['sell_value'] = randint(manx_rent, 500)
-        land_property['rent_value'] = randint(50, manx_rent)
+        land_property['sell_value'] =  randint(manx_rent, 200)
+        land_property['rent_value'] = randint(20, manx_rent)
         land_property['position'] = randint(1, 20)
 
         return  land_property
@@ -57,22 +56,12 @@ class PalyerManager(object):
         return 100
 
 
-    def lost_game(self):
-        pass
-        # saldo negativo
-
-
-    def game_over(self):
-        # 1 jogador com saldo positivo
-        pass
-
-
     def define_player(self, player_numer):
         """ 4 types
-            Player one is impulsive
-            Player two is demanding;
-            Player three is cautious;
-            Player four is random;
+            1. Player one is impulsive
+            2. Player two is demanding;
+            3. Player three is cautious;
+            4. Player four is random;
         """
 
         player_dict = {}
@@ -85,29 +74,33 @@ class PalyerManager(object):
         return player_dict[str(player_numer)]
 
 
-    def buy_land_property_player_1(self, player_number):
+    def buy_land_property_player_1(self, player_number, all_player_info):
         print_log(f'IMPULSIVE PALYER BUYS ANY PROPERTY WHEREEVER HE STAND ON')
 
         property_dict = self.get_land_property()
 
         property_dict['name'] = 'IRAQUE'
         property_dict['position'] = self.position
-        balance = self.money - property_dict['sell_value']
 
-        player_content = {
-            'name': self.player,
-            'current_property': property_dict,
-            'balance': balance,
-            'position': self.position
-        }
+        try:
+            player_info = all_player_info[str(player_number)]
+        except Exception as err:
+            player_info = self.get_player_info()
+
+        old_balance = player_info['balance']
+        new_balance = old_balance - property_dict['sell_value']
+
+        player_info['current_property'] = property_dict
+        player_info['balance'] = new_balance
+
         player_full_content = {
-            str(player_number): player_content
+            str(player_number): player_info
         }
 
-        return player_full_content
+        return player_info, player_full_content
 
 
-    def buy_land_property_player_2(self, player_number):
+    def buy_land_property_player_2(self, player_number, all_player_info):
         print_log(f'DEMANDING PALYER BUYS ANY PROPERTY AS LONG AS ITS RENT IS GREATER THAN 50.')
 
         property_dict = self.get_land_property()
@@ -116,38 +109,51 @@ class PalyerManager(object):
 
             property_dict['name'] = 'USA'
             property_dict['position'] = self.position
-            balance = self.money - property_dict['sell_value']
 
-            player_info = self.get_player_info()
+            try:
+                player_info = all_player_info[str(player_number)]
+            except Exception as err:
+                player_info = self.get_player_info()
+
+            old_balance = player_info['balance']
+            new_balance = old_balance - property_dict['sell_value']
 
             player_info['current_property'] = property_dict
-            player_info['balance'] = balance
+            player_info['balance'] = new_balance
 
             player_full_content = {
                 str(player_number): player_info
             }
 
         else:
-            player_info = self.get_player_info()
+            try:
+                player_info = all_player_info[str(player_number)]
+            except Exception as err:
+                player_info = self.get_player_info()
+
             player_full_content = 0
 
         return player_info, player_full_content
 
 
     def buy_land_property_player_3(self, player_number):
-        print_log(f'CAUTIOUS PALYER BUYS ANY PROPERTY AS LONG AS HE HAS A \n   RESERVE OF 80 BALANCE LEFT AFTER THE PURCHASE IS MADE. ')
+        print_log(f'CAUTIOUS PALYER BUYS ANY PROPERTY AS LONG AS HE HAS A \n   RESERVE OF 80 BALANCE LEFT AFTER THE PURCHASE IS MADE.')
 
         property_dict = self.get_land_property()
-        balance = self.money - property_dict['sell_value']
+        new_balance = self.money - property_dict['sell_value']
 
-        if balance == 80:
+        print('-'*80)
+        print(f'\n player 3: new_balance = {new_balance} \n')
+        print('-'*80)
+
+        if new_balance == 80:
             property_dict['name'] = 'FRANCE'
             property_dict['position'] = self.position
 
             player_info = self.get_player_info()
 
             player_info['current_property'] = property_dict
-            player_info['balance'] = balance
+            player_info['balance'] = new_balance
 
             player_full_content = {
                 str(player_number): player_info
@@ -160,29 +166,41 @@ class PalyerManager(object):
         return player_info, player_full_content
 
 
-    def buy_land_property_player_4(self, player_number):
+    def buy_land_property_player_4(self, player_number, all_player_info):
         print_log(f'RANDOM PALYER BUYS BUYS THE PROPERTY HE LANDS ON WITH A 50% PROBABILITY')
 
-        buy = randint(1, 100)
+        buy = randint(1, 50)
+        print('-'*80)
+        print(f'\n player 4 : probabilidade = {buy}\n')
+        print('-'*80)
 
         if buy == 50:      # 50%
             property_dict = self.get_land_property()
-            balance = self.money - property_dict['sell_value']
 
             property_dict['name'] = 'FRANCE'
             property_dict['position'] = self.position
 
-            player_info = self.get_player_info()
+            try:
+                player_info = all_player_info[str(player_number)]
+            except Exception as err:
+                player_info = self.get_player_info()
+
+            old_balance = player_info['balance']
+            new_balance = old_balance - property_dict['sell_value']
 
             player_info['current_property'] = property_dict
-            player_info['balance'] = balance
+            player_info['balance'] = new_balance
 
             player_full_content = {
                 str(player_number): player_info
             }
 
         else:
-            player_info = self.get_player_info()
+            try:
+                player_info = all_player_info[str(player_number)]
+            except Exception as err:
+                player_info = self.get_player_info()
+
             player_full_content = 0
 
         return player_info, player_full_content
@@ -190,19 +208,29 @@ class PalyerManager(object):
 
     def pay_rent_for_property(self, player_number, all_player_info, property_board_list):
 
-        # import pdb; pdb.set_trace()
-        other_player_property = property_board_list[self.position]
-        player_info = all_player_info.get(str(player_number))
+        other_player_property = property_board_list[self.position-1]
+
+        owner_id_list = other_player_property.keys()
+        owner_id_list = list(owner_id_list)
+        owner_id = owner_id_list[0]
+
+        try:
+            player_info = all_player_info[str(player_number)]
+        except Exception as err:
+            player_info = self.get_player_info()
 
         balance = player_info['balance']
+
         # pay for property
-        pay = balance - other_player_property[str(player_number)]['rent_value']
-        other_player_property[str(player_number)]['balance'] += (
-            other_player_property[str(player_number)]['balance']
+        rent_value = other_player_property[owner_id]['current_property']['rent_value']
+
+        pay = balance - rent_value
+
+        other_player_property[owner_id]['current_property']['rent_value'] += (
+            rent_value
         )
 
         player_info['balance'] = pay
-        # all_player_info[str(player_number)] = player_info
 
         print(other_player_property)
         print(all_player_info)
